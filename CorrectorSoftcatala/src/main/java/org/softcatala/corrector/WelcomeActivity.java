026a6c4a3b39244fb3b1e7c63052e074b2b46197
Jordi Mas <jmas@softcatala.org>
2018-10-21 17:25:33 +0200

    Dialog box explaining how to install the spell checker

---
+++ CorrectorSoftcatala/src/main/java/org/softcatala/corrector/WelcomeActivity.java
@@ -0,0 +1,36 @@
+package org.softcatala.corrector;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        String steps = getResources().getString(R.string.install_steps);
+        TextView textView = findViewById(R.id.textViewSteps);
+        textView.setText(steps);
+
+        String limitations = getResources().getString(R.string.app_limitations);
+        textView = findViewById(R.id.textViewLimitations);
+        textView.setText(limitations);
+
+        Button okButton = findViewById(R.id.buttonClose);
+        String text = getResources().getString(R.string.button_close);
+        okButton.setText(text);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
