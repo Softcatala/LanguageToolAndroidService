e3def02fa0d4fd087cd0881cdfe7892fbf71f81f
Jordi Mas <jmas@softcatala.org>
2018-10-24 08:05:14 +0200

    Copy translations to target

--- i18n_update.sh
+++ i18n_update.sh
@@ -5,19 +5,17 @@
 USERNAME=jordimash
 PASSWORD=`cat ~/.transifex_password`
 
-rm -I i18n-temp
-mkdir i18n-temp
-cd i18n-temp
 
 # list of languages in the same order as on https://www.transifex.com/projects/p/languagetool/:
 for lang in en ast be br ca zh da nl eo fr gl de el_GR it pl ru sl es tl uk ro sk cs sv is lt km pt_PT pt_BR fa
 do
   SOURCE=downloaded.tmp
-  curl --user $USERNAME:$PASSWORD https://www.transifex.com/api/2/project/languagetool/resource/lt-for-android/translation/$lang/?file >strings-$lang.xml
+  curl --user $USERNAME:$PASSWORD https://www.transifex.com/api/2/project/languagetool/resource/lt-for-android/translation/$lang/?file >$SOURCE
   localLang=$lang
   if [ $lang = 'pt_PT' ]; then
     # special case: if this is named pt_PT it never becomes active because we use "lang=xx" links
     # in the web app that don't contain the country code:
     localLang=pt
   fi
+  cp $SOURCE CorrectorSoftcatala/src/main/res/values-$localLang/strings.xml
 done
